c8a2ca5b3685ba743bee2ff1a37935b8e97a1486
xulejun <xulejun@mysteel.com>
2021-04-29 11:00:27 +0800

    feat：JVM样例代码

---
+++ src/main/java/com/xlj/tools/jvm/VolatileDemo.java
@@ -0,0 +1,43 @@
+package com.xlj.tools.jvm;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Volatile可见性，不保证原子性，禁止指令重排序
+ *
+ * @author xlj
+ * @date 2020/11/8 15:12
+ */
+
+class MyNumber {
+//    int number = 10;
+    volatile int number = 10;
+
+    public void doNumber() {
+        this.number = 1024;
+    }
+}
+
+public class VolatileDemo {
+    public static void main(String[] args) {
+        MyNumber myNumber = new MyNumber();
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " is running");
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            myNumber.doNumber();
+        }, "A").start();
+
+        // 没加volatile关键字，不可见，其他线程接收不到通知，持续循环等待
+        while (myNumber.number == 10) {
+        }
+
+        // number改变时结束
+        System.out.println(Thread.currentThread().getName() + " is over");
+    }
+
+
+}
