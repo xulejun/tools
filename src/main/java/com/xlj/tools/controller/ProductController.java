827167320d156f0126bb224c346c17469435a0e7
xulejun <xu-lejun@qq.com>
2021-05-01 07:40:35 +0800

    fix：商品编辑bug修复

--- src/main/java/com/xlj/tools/controller/ProductController.java
+++ src/main/java/com/xlj/tools/controller/ProductController.java
@@ -7,10 +7,7 @@ import com.xlj.tools.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
