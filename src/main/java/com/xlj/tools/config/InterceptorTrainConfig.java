8a57dba31f473ddd94acddd5ecdd71d6095e3be6
xulejun <xu-lejun@qq.com>
2021-05-05 10:19:38 +0800

    style：关闭拦截登录配置

--- src/main/java/com/xlj/tools/config/InterceptorTrainConfig.java
+++ src/main/java/com/xlj/tools/config/InterceptorTrainConfig.java
@@ -7,19 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * 拦截器链
+ * 登录拦截验证时打开此类
  *
  * @author xlj
  * @date 2021/4/29
  */
-@Configuration
-public class InterceptorTrainConfig implements WebMvcConfigurer {
-    /**
-     * 把拦截器添加到拦截器链中
-     *
-     * @param registry
-     */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new SourceAccessInterceptor()).addPathPatterns("/**");
-    }
-}
+//@Configuration
+//public class InterceptorTrainConfig implements WebMvcConfigurer {
+//    /**
+//     * 把拦截器添加到拦截器链中
+//     *
+//     * @param registry
+//     */
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new SourceAccessInterceptor()).addPathPatterns("/**");
+//    }
+//}
