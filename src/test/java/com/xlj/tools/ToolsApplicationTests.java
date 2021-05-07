efc7cc395d01ea670abd3c4bb6118f91b3212c2c
xulejun <xulejun@mysteel.com>
2021-05-07 20:08:55 +0800

    feat：性能计算

--- src/test/java/com/xlj/tools/ToolsApplicationTests.java
+++ src/test/java/com/xlj/tools/ToolsApplicationTests.java
@@ -1,9 +1,12 @@
 package com.xlj.tools;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.TimeInterval;
 import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.xlj.tools.bean.User;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,8 +14,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 //@SpringBootTest
+@Slf4j
 public class ToolsApplicationTests {
 
     @Test
@@ -24,6 +29,36 @@ public class ToolsApplicationTests {
         }
         JSONArray objects = new JSONArray(list);
         List<User> users = JSONUtil.toList(objects, User.class);
+    }
+
+    /**
+     * HuTool性能统计
+     */
+    @Test
+    public void test8() {
+        int loopcount = 2;
+        TimeInterval timer = DateUtil.timer();
+        forloop(loopcount);
+        long interval1 = timer.interval();
+        log.info("性能统计，总共花费了{} (毫秒数)", interval1);
+
+        forloop(loopcount);
+        long interval2 = timer.intervalRestart();
+        log.info("性能统计，总共花费了 {}(毫秒数),并重置", interval2);
+
+        forloop(loopcount);
+        long interval3 = timer.interval();
+        log.info("性能统计，总共花费了 {}(毫秒数)", interval3);
+    }
+
+    private void forloop(int total) {
+        for (int i = 0; i < total; i++) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
