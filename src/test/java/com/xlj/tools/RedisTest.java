ce2ba15bd584dfc6038fc70917088b4efefb3c0a
lejunxu <xu-lejun@qq.com>
2021-09-26 10:36:58 +0800

    add：jedis操作zset数据类型

--- src/test/java/com/xlj/tools/RedisTest.java
+++ src/test/java/com/xlj/tools/RedisTest.java
@@ -7,10 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Tuple;
 
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author xlj
@@ -30,8 +38,41 @@ public class RedisTest {
         System.out.println(hello);
         if (hello.isPresent()) {
             System.out.println("存在");
-        }else {
+        } else {
             System.out.println("不存在");
         }
+    }
+
+    @Test
+    public void testZSet() throws Exception {
+        String key = "ocr:user:config";
+        String value = "config";
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+//        for (int i = 0; i < 5; i++) {
+//            // 添加 key，score，value
+//            jedis.zadd(key, System.currentTimeMillis(), value + i);
+//            TimeUnit.SECONDS.sleep(1L);
+//        }
+        // 查询所有 value 带 score
+        for (Tuple tuple : jedis.zrangeWithScores(key, 0, -1)) {
+            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(Long.parseLong(String.valueOf(BigDecimal.valueOf(tuple.getScore()))))));
+        }
+        System.out.println(jedis.zrangeWithScores(key, 0, -1));
+        String[] a = new String[2];
+        a[0] = Arrays.toString(jedis.zrange(key, 3, 3).toArray());
+        a[1] = jedis.zrange(key, 3, 3).toArray()[0].toString();
+//        System.out.println(a[0]);
+//        System.out.println(a[1]);
+        // 通过 key 和 score 获取 value
+        System.out.println("通过 key 和 score 获取 value:" + jedis.zrangeByScore(key, 1632471636911L, 1632471636911L));
+        // 删除排行指定区间所有value
+//        jedis.zremrangeByRank(key, 0, 0);
+//        jedis.zrangeWithScores(key, 0, -1).forEach(System.out::println);
+        // 获取key中元素的个数
+        System.out.println(jedis.zcard(key).toString());
+        String date = "2021-08-19 12:00:00";
+        long time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(date).getTime();
+        System.out.println(time);
+
     }
 }
