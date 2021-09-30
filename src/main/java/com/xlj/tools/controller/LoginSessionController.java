471e4f7be045c0292db57aea1d3a5074beae78ab
lejunxu <xu-lejun@qq.com>
2021-09-30 15:38:16 +0800

    feat：统一session登录扩展

--- src/main/java/com/xlj/tools/controller/LoginSessionController.java
+++ src/main/java/com/xlj/tools/controller/LoginSessionController.java
@@ -1,12 +1,18 @@
 package com.xlj.tools.controller;
 
-import com.xlj.tools.bean.User;
+import cn.hutool.core.lang.UUID;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.StrUtil;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * spring-session-redis 统一存储
@@ -15,18 +21,64 @@ import javax.servlet.http.HttpSession;
  * @date 2021/9/29
  */
 @Controller
+@RequestMapping("/session")
 public class LoginSessionController {
+    @Autowired
+    StringRedisTemplate redisTemplate;
 
-    @PostMapping("/doLogin")
-    @ResponseBody
-    public String loginSession(String username, HttpSession session) {
-        User user = User.builder().id(1).name(username).build();
-        session.setAttribute("loginUser", user);
-        return "你好：" + user.getName();
+    /**
+     * 处理登录请求
+     *
+     * @param username
+     * @param session
+     * @return
+     */
+    @PostMapping(value = "/doLogin")
+    public String loginSession(String username, String password, @RequestParam("path") String path, HttpSession session) {
+        if (StrUtil.isNotBlank(username) && StrUtil.isNotBlank(password)) {
+            String uuid = IdUtil.simpleUUID();
+            redisTemplate.opsForValue().set(uuid, username);
+            return "redirect:" + path + "?token=" + uuid;
+        }
+        return "loginSession";
     }
 
+    /**
+     * 页面跳转
+     *
+     * @return
+     */
     @GetMapping("loginSession")
-    public String loginSession() {
-        return "loginSession";
+    public String loginSession(@RequestParam("path") String path, Model model) {
+        model.addAttribute("path", path);
+        return "/session/login";
+    }
+
+    /**
+     * 登录后获取员工列表
+     *
+     * @param model
+     * @param httpSession
+     * @return
+     */
+    @GetMapping("employees")
+    public String employees(Model model, HttpSession httpSession,
+                            @RequestParam(value = "token", required = false) String token) {
+        if (StrUtil.isNotBlank(token)) {
+            httpSession.setAttribute("loginUser", token);
+        }
+
+        if (Objects.isNull(httpSession.getAttribute("loginUser"))) {
+            // 将自身url通过参数进行传递，后续流程实现跳转
+            return "redirect:http://127.0.0.1:8080/session/loginSession" + "?path=employees";
+        } else {
+//            String username = redisTemplate.opsForValue().get(token);
+            List<String> list = Lists.newArrayList();
+            list.add("张三");
+            list.add("李四");
+            model.addAttribute("list", list);
+            model.addAttribute("username", "xlj");
+            return "/session/listEmployees";
+        }
     }
 }
