1571386a374b6144e567587c171319bf1f58017c
xulejun <1142858332@qq.com>
2021-11-28 13:59:15 +0800

    feat：建表语句

---
+++ src/main/resources/doc/createTable.sql
@@ -0,0 +1,32 @@
+create schema personal collate utf8_general_ci;
+
+-- file_info: table
+CREATE TABLE `file_info`
+(
+    `id`   int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) DEFAULT NULL COMMENT '文件名',
+    `path` varchar(255) DEFAULT NULL COMMENT '路径',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8 COMMENT ='文件信息';
+
+-- product: table
+CREATE TABLE `product`
+(
+    `id`    int(11) NOT NULL AUTO_INCREMENT,
+    `name`  varchar(100)   DEFAULT NULL COMMENT '商品名称',
+    `price` decimal(10, 0) DEFAULT NULL COMMENT '价格',
+    `stock` varchar(255)   DEFAULT NULL COMMENT '秒杀随机码',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+-- user: table
+CREATE TABLE `user`
+(
+    `id`   int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) DEFAULT NULL COMMENT '用户名',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
