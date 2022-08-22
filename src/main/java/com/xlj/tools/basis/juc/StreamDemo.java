29f42640edd987eb1d3ceadc952566d236f63c06
lejunxu <xu-lejun@qq.com>
2022-08-22 15:13:51 +0800

    feat：流式计算

--- src/main/java/com/xlj/tools/basis/juc/StreamDemo.java
+++ src/main/java/com/xlj/tools/basis/juc/StreamDemo.java
@@ -2,6 +2,8 @@ package com.xlj.tools.basis.juc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Stream流式计算------可代替数据库查询
@@ -23,18 +25,39 @@ public class StreamDemo {
         list.add(personBo3);
         list.add(personBo4);
 
-        // 集合stream流式计算————filter过滤（返回还是对象）
+        // filter 过滤（返回还是对象）
         list.stream().filter(t -> {
             return t.getId() % 2 == 0;
         }).forEach(System.out::println);
 
-//        // 集合stream流式计算————map映射（返回为单个值）
-//        list.stream().map(t -> {return t.getId();}).forEach(System.out::println);
-//
-//        // limit限制数————collect将map单个映射转为list集合
-//        System.out.println(list.stream().map(t -> {
-//            return t.getId();
-//        }).limit(1).collect(Collectors.toList()));
+        // match：anyMatch、allMatch、noneMatch 对流中的元素进行匹配
+        boolean y = list.stream().anyMatch(personBo -> {
+            return personBo.getName().contains("y");
+        });
+
+        // map 将集合中的元素类型，转换成另一种数据类型
+        list.stream().map(PersonBo::getId).forEach(System.out::println);
+
+        // sorted 根据字段属性进行排序
+        list.stream().sorted((u1, u2) -> {
+            return u2.getId().compareTo(u1.getId());
+        }).map(PersonBo::getId).forEach(System.out::println);
+
+        // limit限制数————collect将map单个映射转为list集合
+        list.stream().map(t -> {
+            return t.getId();
+        }).limit(1).forEach(System.out::println);
+
+        // distinct 对流中的元素进行去重
+        list.stream().distinct().forEach(System.out::println);
+
+        // partitioningBy 根据判断的值为true还是false分为两组
+        Map<Boolean, List<PersonBo>> collect = list.stream().collect(Collectors.partitioningBy(personBo -> {
+            return personBo.getId() > 3;
+        }));
+
+        // groupingBy 将数据分组成多个key的形式（即groupby分组）
+        Map<Integer, List<PersonBo>> collect1 = list.stream().collect(Collectors.groupingBy(PersonBo::getId));
 
         // reduce 计算集合总数
         Integer sum = list.stream().map(PersonBo::getId).reduce(Integer::sum).get();
