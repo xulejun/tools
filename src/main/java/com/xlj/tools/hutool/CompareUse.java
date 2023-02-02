dd3f504c223cf32d092a01ec52462b083ce040f5
lejunxu <xu-lejun@qq.com>
2023-02-02 10:55:16 +0800

    feat：计算两个字符串的相似度

--- src/main/java/com/xlj/tools/hutool/CompareUse.java
+++ src/main/java/com/xlj/tools/hutool/CompareUse.java
@@ -3,6 +3,7 @@ package com.xlj.tools.hutool;
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.comparator.PropertyComparator;
 import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -18,17 +19,23 @@ import java.util.List;
  */
 public class CompareUse {
     public static void main(String[] args) {
+        String str1 = "朝阳区安达街旺座国际商务广场斜对面（近轻轨3号线南昌路站）";
+        String str2 = "朝阳区安袄街旺座国际商务广场斜对面(祈轻劲3 地图/导航号线南昌路站)吉大一院、文化广场";
+        // 计算两个字符串的相似度
+        double similar = StrUtil.similar(str1, str2);
+        System.out.println("字符串相似度为：" + similar);
+
         // 根据对象属性进行集合排序
         List<Hero> heros = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             heros.add(new Hero("hero " + i, RandomUtil.randomInt(100)));
         }
-        System.out.println("未排序的集合:");
-        System.out.println(CollectionUtil.join(heros, "\r\n"));
+//        System.out.println("未排序的集合:");
+//        System.out.println(CollectionUtil.join(heros, "\r\n"));
 
         Collections.sort(heros, new PropertyComparator<>("hp"));
-        System.out.println("根据属性 hp 排序之后：");
-        System.out.println(CollectionUtil.join(heros, "\r\n"));
+//        System.out.println("根据属性 hp 排序之后：");
+//        System.out.println(CollectionUtil.join(heros, "\r\n"));
     }
 
 
